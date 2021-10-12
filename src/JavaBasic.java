ca7dc9ae9d5bed07c8f1995b358af40bcefc1105
sitymarliana <msitimarliana@gmail.com>
2021-10-12 17:16:00 +0700

    challenge 1 java ndp

---
+++ src/JavaBasic.java
@@ -0,0 +1,17 @@
+public class JavaBasic {
+    public static void main(String[] args){
+        String firstName = "Siti";
+        String lastName = " Marliana";
+        int age = 22;
+        String phoneNumber = "+6282285512408";
+        String address = "Batam, Perum Cluster Peony";
+        String status = "Mahasiswa";
+
+        System.out.println("Hallo Nama ku "+firstName+lastName+" :)");
+        System.out.println("Umurku ditahun ini adalah "+age+" tahun");
+        System.out.println("Ingin berkenalan dengan ku? contact aku ya :) di nomor "+phoneNumber);
+        System.out.println("Mungkin pengen gofood in aku ini alamat ku "+address);
+        System.out.println(status+" adalah status ku hehe");
+
+    }
+}
